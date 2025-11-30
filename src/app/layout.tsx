de3084bf6f157bbfc1322b7751f28d03172d2e23
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2025-11-30 06:32:14 +0200

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,33 @@ import { ThemeProvider } from "@/components/theme-provider";
 import PrayerReminder from "@/components/reminders/PrayerReminder";
 import SyncLocalstorageDataProvider from "@/providers/SyncLocalstorageDataProvider";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "ساعتين جد",
+  metadataBase: new URL("https://sa3teen-gad.vercel.app"),
   description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
+  openGraph: {
+    title: "ساعتين جد",
+    description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
+    images: [
+      {
+        url: "https://sa3teen-gad.vercel.app/banners/readme-banner.png", // Full URL
+        width: 1200,
+        height: 630,
+        alt: "ساعتين جد - Pomodoro Timer & Prayer Times",
+      },
+    ],
+    locale: "ar_EG",
+    type: "website",
+    url: "https://sa3teen-gad.vercel.app", // Add this
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ساعتين جد",
+    description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
+    images: ["https://sa3teen-gad.vercel.app/banners/readme-banner.png"], // Full URL
+  },
 };
 
 export default function RootLayout({

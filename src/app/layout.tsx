a173fe4807aa389adf503bace382a7530c8b576a
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2025-11-30 06:25:57 +0200

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import ReduxProvider from "@/providers/ReduxProvider";
 import Navbar from "@/components/navbar/Navbar";
@@ -8,33 +8,11 @@ import { ThemeProvider } from "@/components/theme-provider";
 import PrayerReminder from "@/components/reminders/PrayerReminder";
 import SyncLocalstorageDataProvider from "@/providers/SyncLocalstorageDataProvider";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
-// export const metadata: Metadata = {
-//   title: "ساعتين جد",
-//   metadataBase: new URL("https://sa3teen-gad.vercel.app"),
-//   description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
-//   openGraph: {
-//     title: "ساعتين جد",
-//     description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
-//     images: [
-//       {
-//         url: "/banners/readme-banner.png",
-//         width: 1200,
-//         height: 630,
-//         alt: "ساعتين جد - Pomodoro Timer & Prayer Times",
-//       },
-//     ],
-//     locale: "ar_EG",
-//     type: "website",
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "ساعتين جد",
-//     description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
-//     images: ["/banners/readme-banner.png"],
-//   },
-// };
+export const metadata: Metadata = {
+  title: "ساعتين جد",
+  description: "ساعتين شاى وكوباية جد وكله هيبقا تمام",
+};
 
 export default function RootLayout({
   children,
@@ -45,16 +23,7 @@ export default function RootLayout({
   return (
     <html lang="ar" suppressHydrationWarning>
 
-      <Head>
-        <title>ساعتين جد</title>
-        <meta name="description" content="ساعتين شاى وكوباية جد وكله هيبقا تمام" />
-        <meta property="og:title" content="ساعتين جد" />
-        <meta property="og:description" content="ساعتين شاى وكوباية جد وكله هيبقا تمام" />
-        <meta property="og:image" content="/banners/readme-banner.png" />
-        {/* <meta property="og:url" content="https://sa3teen-gad.vercel.app" /> */}
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="ar_EG" />
-      </Head>
+
       <ReduxProvider>
         <body dir="rtl" className="bg-background text-foreground transition-colors duration-300 md:pb-0 pb-16">
           <ThemeProvider

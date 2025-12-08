56f63ea0682d5b456ac1271a9fd7f795544bb2d4
abdelrahmanmostafa0 <abdelrahmanmostafa.developer@gmail.com>
2025-12-08 22:17:27 +0200

    chore: add mongoose types and refactor user model and pomodora model

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,3 @@
-import { NextRequest } from "next/server";
 import { loginHandler } from "@/lib/handlers/auth/login.handler";
 
 export const POST = loginHandler;
--- src/lib/models/PomodoroSessions.ts
+++ src/lib/models/PomodoroSessions.ts
@@ -1,32 +1,37 @@
 import { Schema, model, models } from "mongoose";
-interface IPauseInterval {
-  pauseStart: Date;
-  pauseEnd: Date;
-}
-export interface IPomodoroSession {
-  userId: string;
-  startTime: Date;
-  endTime: Date;
-  durationMinutes: number;
-  taskName?: string;
-  isPomodoro: boolean;
-  pauseIntervals?: IPauseInterval[];
-}
 
-const PomodoroSessionSchema: Schema<IPomodoroSession> = new Schema(
+const PauseIntervalSchema = new Schema({
+  pauseStart: { type: Date, required: true },
+  pauseEnd: { type: Date, default: null },
+});
+
+const PomodoroSessionSchema = new Schema(
   {
-    userId: { type: String, required: true },
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true },
-    durationMinutes: { type: Number, required: true },
+    endTime: { type: Date, default: null },
+    durationMinutes: { type: Number, default: null },
     taskName: { type: String, default: null },
-    pauseIntervals: { type: [Object], default: [] },
     isPomodoro: { type: Boolean, default: true },
+    pauseIntervals: { type: [PauseIntervalSchema], default: [] },
   },
   { timestamps: true }
 );
 
 PomodoroSessionSchema.index({ userId: 1, startTime: 1 });
+
+PomodoroSessionSchema.pre("save", function () {
+  if (this.endTime) {
+    const total = this.endTime.getTime() - this.startTime.getTime();
+
+    const pauseMs = this.pauseIntervals.reduce((acc, p) => {
+      if (!p.pauseEnd) return acc;
+      return acc + (p.pauseEnd.getTime() - p.pauseStart.getTime());
+    }, 0);
+
+    this.durationMinutes = Math.max(0, Math.floor((total - pauseMs) / 60000));
+  }
+});
 
 export const PomodoroSession =
   models.PomodoroSession || model("PomodoroSession", PomodoroSessionSchema);
